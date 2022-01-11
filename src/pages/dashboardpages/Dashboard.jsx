fca7b384f628b7a7397a983ae984ffface307d87
ysoni-dev <ysoni1614@gmail.com>
2022-01-11 15:01:58 +0530

    fe

---
+++ src/pages/dashboardpages/Dashboard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { enrolled } from "../../services/api";
+
+import img from '../images/dash.png'
+
+const Dashboard=()=>{
+
+  useEffect(()=>{
+    enrolledData()
+},[])
+
+const {token} = useParams()
+
+const enrolledData=async()=>{
+    const res = await enrolled(token)
+    console.log('enrlled')
+}
+    return(
+        <>
+     
+        <div  className="row">
+          <div className="col-2"></div>
+         
+        <div class="col-10" >
+        <h4 className="">Recent Activity</h4>
+    <div class="card" style={{border:'none'}} >
+      <div class="card-body">
+      <img src={img} height="200" width="300" class="" alt="..."></img>
+        <h5 class="card-title">Introduction to MachineLearning </h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       
+      </div>
+    </div>
+    
+    
+  </div>
+        </div>
+        </>
+    )
+}
+
+export default Dashboard
